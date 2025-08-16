2188c1a213a80d69fc2144e81b6fbd5b4d9d6193
akshat <kshatsharmaaa@gmail.com>
2025-08-16 15:58:19 +0530

    navbar dependency bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import Navbar from './components/NAvbar'
+import Navbar from './components/Navbar'
 import Hero from './components/Hero';
 import TrustedBy from './components/TrustedBy';
 import Services from './components/Services';
